b108293bb6be850bed93e0fde51a58f60015fd07
Basirul Khan <141140812+basirulkhan6646@users.noreply.github.com>
2024-05-03 11:01:49 +0530

    new plugin to create product including base64 url
    Convert base64 image URL  and upload in product using Product API

---
+++ bk-product-base64-image/bk-product-base-image.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * Plugin Name: Base64 Image Upload using Product API
+ * Plugin URI: https://github.com/basirulkhan6646
+ * Description:Plugin for woocommerce product image upload using base64 url using product api
+ * Author: Basirul Khan
+ * Version: 1.0
+ * Text Domain: bkwoo
+ */
+
+add_action('rest_api_init', 'aec_woocommerce_api_customize');
+
+function aec_woocommerce_api_customize()
+{
+
+    add_action( 'woocommerce_rest_insert_product_object', 'custom_product_api_image_upload', 10, 2 );
+}
+
+
+function custom_product_api_image_upload( $product, $request){
+
+    if ( isset( $request['image_base64_url'] ) && ! empty( $request['image_base64_url'] ) ) {
+
+        $image_data = base64_decode( $request['image_base64_url'] );
+        $product_name= preg_replace('/[^a-z0-9-]+/', '-', strtolower(str_replace(' ', '-', $request['name'])));
+        $upload_dir = wp_upload_dir();
+        $image_filename = wp_unique_filename( $upload_dir['path'], $product_name.'.png' );
+        $image_path = $upload_dir['path'] . '/' . $image_filename;
+
+        file_put_contents( $image_path, $image_data );
+        $attachment = array(
+            'post_title'     => sanitize_file_name( $image_filename ),
+            'post_mime_type' => 'image/png',
+            'post_status'    => 'inherit'
+        );
+
+        $attachment_id = wp_insert_attachment( $attachment, $image_path, $product->get_id() );
+        if ( ! is_wp_error( $attachment_id ) ) {
+
+            require_once( ABSPATH . 'wp-admin/includes/image.php' );
+            $attachment_metadata = wp_generate_attachment_metadata( $attachment_id, $image_path );
+            wp_update_attachment_metadata( $attachment_id, $attachment_metadata );
+
+            $product->set_image_id( $attachment_id );
+            $product->save();
+        }
+    }
+}
